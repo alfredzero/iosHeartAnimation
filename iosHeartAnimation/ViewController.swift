6d8516e4238754b662fe755c17bbcd0c9ea37a86
alfredzero <amezcuaalfredo81@gmail.com>
2024-04-28 23:35:25 -0700

    ani heart

--- iosHeartAnimation/ViewController.swift
+++ iosHeartAnimation/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let imageAnimation: UIImageView = {
+        let ani = UIImageView()
+        ani.isUserInteractionEnabled = true
+        ani.image = UIImage(named: "tile00")
+        ani.translatesAutoresizingMaskIntoConstraints = false
+        return ani
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        view.addSubview(imageAnimation)
+        imageAnimation.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(animate)))
+        imageAnimation.heightAnchor.constraint(equalToConstant: 75).isActive = true
+        imageAnimation.widthAnchor.constraint(equalToConstant: 75).isActive = true
+        imageAnimation.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageAnimation.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        for i in 0..<29 {
+                   print("tile0\(i)")
+                   motionClip.append(UIImage(named: "tile0\(i)")!)
+        }
     }
+    
+    var motionClip = [UIImage]()
 
-
+    @objc func animate() {
+        print("heart swoosh")
+        imageAnimation.animationImages = motionClip
+        imageAnimation.animationDuration = 1
+        imageAnimation.animationRepeatCount = 1
+        imageAnimation.startAnimating()
+    }
 }
 
